670259f46fb824caf0c7639ab54eeb4655a7f0ec
Yami Odymel <yamiodymel@gmail.com>
2016-07-18 14:48:41 +0000

    Added tests for multi keys

--- test/hoffTest.php
+++ test/hoffTest.php
@@ -18,5 +18,95 @@ class HoffTest extends \PHPUnit_Framework_TestCase
         
         $this->hoff->create('test_table');
     }
+    
+    function testTableType()
+    {
+        $this->hoff->column('test')->varchar(32);
+        $this->hoff->MyISAM()->create('test_myisam_table');
+        
+        $this->hoff->column('test')->varchar(32)
+                   ->InnoDB()->create('test_innodb_table');
+    }
+    
+    function testPrimaryKey()
+    {
+        $this->hoff->column('test')->varchar(32)->primary()
+                   ->column('test2')->varchar(32)
+                   ->create('test_table2');
+    }
+    
+    function testNamingPrimaryKey()
+    {
+        $this->hoff->column('test')->varchar(32)
+                   ->column('test2')->varchar(32)
+                   ->primary('pk_test', ['test', 'test2'])
+                   ->create('test_table3');
+    }
+    
+    function testMultiPrimaryKey()
+    {
+        $this->hoff->column('test')->varchar(32)
+                   ->column('test2')->varchar(32)
+                   ->primary(['test', 'test2'])
+                   ->create('test_table3');
+    }
+    
+    function testUniqueKey()
+    {
+        $this->hoff->column('test')->varchar(32)->unique()
+                   ->column('test2')->varchar(32)
+                   ->create('test_table4');
+    }
+    
+    function testNamingUniqueKey()
+    {
+        $this->hoff->column('test')->varchar(32)
+                   ->column('test2')->varchar(32)
+                   ->column('test3')->varchar(32)
+                   ->column('test4')->varchar(32)
+                   ->unique('uk_test', ['test', 'test2'])
+                   ->unique('uk_test2', ['test3', 'test4'])
+                   ->create('test_table5');
+    }
+    
+    function testMultiUniqueKey()
+    {
+        $this->hoff->column('test')->varchar(32)
+                   ->column('test2')->varchar(32)
+                   ->column('test3')->varchar(32)
+                   ->column('test4')->varchar(32)
+                   ->unique(['test', 'test2'])
+                   ->unique(['test3', 'test4'])
+                   ->create('test_table5');
+    }
+    
+    function testIndexKey()
+    {
+        $this->hoff->column('test')->varchar(32)->index()
+                   ->column('test2')->varchar(32)
+                   ->create('test_table6');
+    }
+    
+    function testNamingIndexKey()
+    {
+        $this->hoff->column('test')->varchar(32)
+                   ->column('test2')->varchar(32)
+                   ->column('test3')->varchar(32)
+                   ->column('test4')->varchar(32)
+                   ->index('ik_test', ['test', 'test2'])
+                   ->index('ik_test2', ['test3', 'test4'])
+                   ->create('test_table7');
+    }
+    
+    function testMultiIndexKey()
+    {
+        $this->hoff->column('test')->varchar(32)
+                   ->column('test2')->varchar(32)
+                   ->column('test3')->varchar(32)
+                   ->column('test4')->varchar(32)
+                   ->index(['test', 'test2'])
+                   ->index(['test3', 'test4'])
+                   ->create('test_table7');
+    }
 }
 ?>
